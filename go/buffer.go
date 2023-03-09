f5315837c8e6a70c8b2305988b46e48197dcf914
Jossué Dután <jossue.dutan@kushkipagos.com>
2023-03-09 17:30:59 -0500

    logger example

--- go/buffer.go
+++ go/buffer.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"log"
 )
 
@@ -10,4 +11,22 @@ func main() {
 	var RoleLevel int
 
 	logger := log.New(&buf, "logger: ", log.Lshortfile)
+
+	fmt.Println("Please enter your user level")
+	fmt.Scanf("%d", &RoleLevel)
+
+	switch RoleLevel {
+	case 1:
+		// Log successful login
+		logger.Printf("Login successful.")
+		fmt.Print(&buf)
+	case 2:
+		// Log unsuccessful Login
+		logger.Printf("Login unsuccessful - Insufficient access level.")
+		fmt.Print(&buf)
+	default:
+		// Unspecified error
+		logger.Print("Login error.")
+		fmt.Print(&buf)
+	}
 }
